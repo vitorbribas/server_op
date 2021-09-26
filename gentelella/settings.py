8044b314ed3cad4ac04fa3eed2e279184360984a
Helio Adson <helioadson@gmail.com>
2021-09-26 19:28:18 -0300

    fix: changing django-heroku config to after all others configs

--- gentelella/settings.py
+++ gentelella/settings.py
@@ -16,8 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-django_heroku.settings(locals())
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -156,3 +154,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store you
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
